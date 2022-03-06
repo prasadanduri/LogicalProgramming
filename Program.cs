3ee22714c559fe57985701e61e7c5fd2e69661b3
prasad <prasadanduri13gmail.com>
2022-03-06 10:15:34 +0530

    check an alphabet is Vovel or Constent

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LogicalPorgramming22
+{
+    class Program
+    {
+        static void Main (string[]arg)
+        {
+            VovelOrConstent.CheckAlphabetVovelOrConstent();
+        }
+    }
+       
+
+}
