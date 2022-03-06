7d45b54de292e4d4044d3e569a01b480c86c317f
prasad <prasadanduri13gmail.com>
2022-03-06 11:13:46 +0530

    Find the largest among the three numbers

---
+++ LargestNumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPorgramming22
+{
+    public class LargestNumber
+    {
+        public static void FindTheLargestNumber()
+        {
+            int num1, num2, num3;
+            Console.WriteLine("Find the largets of three numbers:");
+            Console.WriteLine("Input the 1st Number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input the 2nd Number");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input the 3rd Number");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            if(num1>num2 && num1>num3)
+            {
+              Console.WriteLine("The 1st number is largest");
+            }
+                else if (num2>num1 && num2>num3 )
+                {
+                    Console.WriteLine("The 2nd number is largest");
+                }
+            else
+            {
+                Console.WriteLine("the 3rd number is largest");
+            }
+              
+        }
+    }
+}
